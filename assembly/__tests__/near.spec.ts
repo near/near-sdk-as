f66f37946655e3eae949cd25298ef490e888d558
Bowen Wang <bowenwang1996@uchicago.edu>
2019-05-15 11:00:16 -0700

    add basic testing

---
+++ assembly/__tests__/near.spec.ts
@@ -0,0 +1,41 @@
+import { base64 } from '../../near';
+
+function stringToBytes(s: string): Uint8Array {
+    let len = s.lengthUTF8 - 1;
+    let res = new Uint8Array(len);
+    let ptr = s.toUTF8();
+    for (let i = 0; i < len; i++) {
+        res[i] = load<u8>(ptr + i);
+    }
+    return res;
+}
+
+function bytesToString(bytes: Uint8Array): string {
+    let res = new Array<string>();
+    for (let i = 0; i < bytes.length; i++) {
+        res.push(String.fromCharCode(bytes[i]));
+    }
+    return res.join('');
+}
+
+function testBase64(original: string, expectedEncoding: string): void {
+    let bytes = stringToBytes(original);
+    let encoded = base64.encode(bytes);
+    expect<string>(encoded).toBe(expectedEncoding);
+    let decoded = base64.decode(encoded);
+    expect<string>(bytesToString(decoded)).toBe(original);
+}
+
+describe('base64 encoding/decoding', (): void => {
+    it('hello world', (): void => {
+        testBase64('hello, world', 'aGVsbG8sIHdvcmxk');
+    });
+
+    it('test pad 1', (): void => {
+        testBase64('some string', 'c29tZSBzdHJpbmc=');
+    });
+
+    it('test pad 2', (): void => {
+        testBase64('some long string', 'c29tZSBsb25nIHN0cmluZw==');
+    });
+});
