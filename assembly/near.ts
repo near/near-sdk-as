296bf5c3c08cf8ae7b57af1c1fae70de168cc61f
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-08-15 15:23:24 -0700

    Remove temporary namespace hack in near.ts -- not needed with package mgmt

--- assembly/near.ts
+++ assembly/near.ts
@@ -1,4 +1,2 @@
 //temporary hack to work around import { storage, near, base64 } from "./near in intermediate main.near.ts files. It doesn't look like this is actually used
-export namespace base64 {}
-export namespace storage {}
 export namespace near {}
