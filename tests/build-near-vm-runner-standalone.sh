c4f378e81171f9a7600f29383b8ff9bdc3d1c876
Bo Yao <bo@nearprotocol.com>
2019-09-05 16:26:15 -0700

    script to download rust and build near vm runner

---
+++ tests/build-near-vm-runner-standalone.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+RUST_VERSION=nightly
+
+git clone https://github.com/nearprotocol/nearcore
+cd nearcore
+
+curl https://sh.rustup.rs -sSf | \
+     sh -s -- -y --no-modify-path --default-toolchain $RUST_VERSION
+
+cargo build -p near-vm-runner-standalone
+
+# RUN apt-get update -qq && apt-get install -y \
+#     git \
+#     cmake \
+#     g++ \
+#     protobuf-compiler \
+#     pkg-config \
+#     libssl-dev \
+#     unzip \
+#     && rm -rf /var/lib/apt/lists/*
+
+# ENV RUSTUP_HOME=/usr/local/rustup \
+#     CARGO_HOME=/usr/local/cargo \
+#     PATH=/usr/local/cargo/bin:$PATH \
+#     RUST_VERSION=nightly
+
+# RUN curl https://sh.rustup.rs -sSf | \
+#     sh -s -- -y --no-modify-path --default-toolchain $RUST_VERSION
+
+# VOLUME [ /near ]
+# WORKDIR /near
+# COPY . .
+
+# ENV CARGO_TARGET_DIR=/tmp/target
+# RUN --mount=type=cache,target=/tmp/target \
+#     --mount=type=cache,target=/usr/local/cargo/git \
+#     --mount=type=cache,target=/usr/local/cargo/registry \
+#     cargo build -p near --release && \
+#     cp /tmp/target/release/near /usr/local/bin/
