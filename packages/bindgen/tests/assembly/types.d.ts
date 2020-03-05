3a659143846017d6db323a54533bee548a350190
Willem Wyndham <willem@cs.umd.edu>
2020-03-05 13:34:14 -0500

    Added lerna support
    -removed test package in favor of individual tests
    - removed bindgen test folder
    - cleaned up types and setup
    - CI now tests everything
    - the stable branch of nearcore is now cloned for runtime tests
    - used shellcheck to improve packages/runtime/tests/test.sh

--- packages/bindgen/tests/assembly/types.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="near-runtime-ts/assembly/as_types" />
-
-interface Object {
-  toJSON(): string;
-}
--- packages/runtime/assembly/as_types.d.ts
+++ packages/runtime/assembly/as_types.d.ts
@@ -1,13 +1,11 @@
+/// <reference types="assemblyscript/std/assembly" />
+
 declare interface Uint8Array {
   dataStart: usize;
 }
 
 declare interface Array<T> {
   dataStart: usize;
-}
-
-declare class Number {
-  toString(): string;
 }
 
 declare function decode<T, K = Uint8Array>(buffer: K, name?: string): T;
