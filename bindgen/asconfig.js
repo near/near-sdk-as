35f4130eaa7dd7d38e29c09b553b131cf276a2a6
Willem Wyndham <willem@cs.umd.edu>
2020-04-21 12:40:59 -0400

    Add tests

--- bindgen/asconfig.js
+++ bindgen/asconfig.js
@@ -11,3 +11,20 @@ compile("../assembly/__tests__/bindgen/test.ts", // input file
         ], {verbose: true});
 
 
+function buildFail(msg, input, output, args, options) {
+        var failed = false;
+        try {
+                compile(input, output, args, options)
+        } catch (err) {
+                failed = true
+        } finally {
+                if (!failed) {
+                        throw new Error(msg);
+                }
+        }
+}
+buildFail("Missing input file should fail")
+buildFail("Missing output file should fail.", "../assembly/__tests__/bindgen/bad.ts")
+buildFail("Failing to compiling should fail.", "../assembly/__tests__/bindgen/bad.ts", "out/bad.wasm", [], { verbose:true })
+console.log("PASSED");
+
