8a8bed463acdefc60ba274dd4c60dd0fddaf0074
Sherif Abushadi <sherif@amgando.org>
2020-05-15 00:52:18 +0300

    add simplest possible test

--- assembly/__tests__/runtime/model.ts
+++ assembly/__tests__/runtime/model.ts
@@ -1,3 +1,10 @@
+import {
+  PersistentDeque,
+  PersistentSet,
+  PersistentVector,
+  PersistentMap,
+} from "../../runtime";
+
 // Exporting a new class TextMessage so it can be used outside of this file.
 @nearBindgen
 export class TextMessage {
@@ -5,4 +12,23 @@ export class TextMessage {
   text: string;
   number: u64;
   isRead: bool;
+}
+
+@nearBindgen
+export class Collections {
+  public getVector() {
+    return new PersistentVector<string>("v");
+  }
+
+  public getMap() {
+    return new PersistentMap<string, string>("m");
+  }
+
+  public getSet() {
+    return new PersistentSet<string>("s");
+  }
+
+  public getDeque() {
+    return new PersistentDeque<string>("dq");
+  }
 }
