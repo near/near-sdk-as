054991bd54d10ea108e919c20d5f85c0b4ea7fed
Willem Wyndham <willem@cs.umd.edu>
2020-07-08 16:21:25 -0400

    Ignore generated artifacts

--- bindgen/dist/typeChecker.d.ts
+++ bindgen/dist/typeChecker.d.ts
@@ -1,9 +1,9 @@
 import { TypeName, Module, Source, Parser } from "visitor-as/as";
 import { BaseVisitor } from "visitor-as";
 export declare class TypeChecker extends BaseVisitor {
-  static floatsFound: string[];
-  check(node: Source): void;
-  static check(node: Parser): void;
-  visitTypeName(node: TypeName): void;
-  static checkBinary(node: Module): void;
+    static floatsFound: string[];
+    check(node: Source): void;
+    static check(node: Parser): void;
+    visitTypeName(node: TypeName): void;
+    static checkBinary(node: Module): void;
 }
