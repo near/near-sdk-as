3782669c3b1cd8beaeafae13e449c82f7a3ed124
Willem Wyndham <willem@cs.umd.edu>
2021-01-06 17:36:29 -0500

    fix: update bignum and add tests to show division works for bigger nums (#364)

---
+++ sdk/assembly/__tests__/bignum.spec.ts
@@ -0,0 +1,20 @@
+import { u128 } from "..";
+
+
+describe("big num division", () => {
+
+  it("should handle division", () => {
+    const mul = u128.mul(
+      u128.from('5000000000000000000000000000'),
+      u128.from(95)
+    );
+  
+    const div100 = u128.div(mul, u128.from(100));
+    expect(div100).toBe(u128.from("4750000000000000000000000000"));
+   
+    const _div10 = u128.div10(mul);
+    const _div100 = u128.div10(_div10);
+    expect(_div100).toBe(div100);
+  
+  });
+});
