995beaa39f2bb540be80dd3b9aa26527f159d46f
Willem Wyndham <willem@cs.umd.edu>
2021-03-10 12:09:46 -0500

    fix: add StaticArray to avoid recomputing array

--- sdk-core/assembly/base64.ts
+++ sdk-core/assembly/base64.ts
@@ -9,29 +9,148 @@ const PADCHAR = "=";
 // @ts-ignore: decorator
 @lazy
 const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
- 
+
 // @ts-ignore: decorator
 @lazy
-const ALPHAVALUES = new Uint8Array(128);
+const ALPHAVALUES = StaticArray.fromArray<u8>([
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 62,
+ 0,
+ 0,
+ 0,
+ 63,
+ 52,
+ 53,
+ 54,
+ 55,
+ 56,
+ 57,
+ 58,
+ 59,
+ 60,
+ 61,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 2,
+ 3,
+ 4,
+ 5,
+ 6,
+ 7,
+ 8,
+ 9,
+ 10,
+ 11,
+ 12,
+ 13,
+ 14,
+ 15,
+ 16,
+ 17,
+ 18,
+ 19,
+ 20,
+ 21,
+ 22,
+ 23,
+ 24,
+ 25,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 26,
+ 27,
+ 28,
+ 29,
+ 30,
+ 31,
+ 32,
+ 33,
+ 34,
+ 35,
+ 36,
+ 37,
+ 38,
+ 39,
+ 40,
+ 41,
+ 42,
+ 43,
+ 44,
+ 45,
+ 46,
+ 47,
+ 48,
+ 49,
+ 50,
+ 51,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+]);
 
 export namespace base64 {
-  // @ts-ignore: decorator
-  @lazy
-  let initialized = false;
 
-  function initialize(): void {
-    for (let n=0;n<ALPHA.length;n++) {
-      ALPHAVALUES[ALPHA.charCodeAt(n)] = n;
-    }
-    initialized = true;
-  }
   
    /**
     * Decode a base64-encoded string and return a Uint8Array.
     * @param s Base64 encoded string.
     */
    export function decode(s: string): Uint8Array {
-     if (!initialized) initialize();
      let i: u32, b10: u32;
      let pads = 0,
          imax = s.length as u32;
