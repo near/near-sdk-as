0334784731ddd5c696f64f5b4bceecee80604f82
Willem Wyndham <willem@cs.umd.edu>
2021-05-12 12:45:46 -0400

    chore: add test and currently fails on u128
    ASON size benefit is seen in number types encoded as strings in json.

--- sdk/assembly/__tests__/ason.spec.ts
+++ sdk/assembly/__tests__/ason.spec.ts
@@ -5,6 +5,8 @@ const foobar = new FooBar();
 describe("compare costs", () => {
   it("compare costs", () => {
     let serailazed = ASON.serialize(foobar);
+    let encoded = encode(foobar);
+    log(`ason: ${serailazed.length} vs JSON: ${encoded.length}`);
     // let asonSeralized = Uint8Array.wrap(serailazed.slice(0).buffer);
     expect(ASON.deserialize<FooBar>(serailazed)).toStrictEqual(foobar);
   });
--- sdk/assembly/__tests__/ason.ts
+++ sdk/assembly/__tests__/ason.ts
@@ -1,24 +1,27 @@
 
-// let uintarray = Uint8Array.wrap([10,199, 99, 88, 100, 255, 100].buffer);
-@ason
+import {u128 } from "..";
+
+let uintarray = Uint8Array.wrap([10,199, 99, 88, 100, 255, 100].buffer);
+
+@nearBindgen
 export class FooBar {
-  // foo: i32 = 0;
-  // bar: u32 = 1;
-  // u64Val: u64 = 4294967297;
-  // u64_zero: u64;
-  // i64Val: i64 = -64;
-  // f32: f32 = 3.14;
-  // f64: f64 = 3.141592653589793238462643383279;
-  // f32_zero: f32;
-  // f64_zero: f64;
-  // flag: bool;
-  // baz: string = "123";
-  // uint8array: Uint8Array = uintarray;
+  foo: i32 = 0;
+  bar: u32 = 1;
+  u64Val: u64 = 4294967297;
+  u64_zero: u64;
+  i64Val: i64 = -64;
+  f32: f32 = 3.14;
+  f64: f64 = 3.141592653589793238462643383279;
+  f32_zero: f32;
+  f64_zero: f64;
+  flag: bool;
+  baz: string = "123";
+  uint8array: Uint8Array = uintarray;
   arr: Array<Array<string>> =  [["hello"], ["hey"]];
   // u32Arr: u32[];
   // i32Arr: i32[];
-  // u128Val: u128;
+  u128Val: u128 = u128.Max;
   // uint8arrays: Array<Uint8Array>;
   // // TODO: Fix u64 arrays
-  // u64Arr: u64[];
+  u64Arr: u64[] = [939628746298746, 64928674298726, 62349867342,9837409238740923874098237, 27346473637222, 939629874623987462987, 49286742987263987, 62349867342,9837409238740923874098237, 27346473637222,939629874623987462987, 649286742987263987, 62349867342,9837409238740923874098237, 27346473637222,939629874623987462987, 649286742987263987, 62349867342,9837409238740923874098237, 27346473637222];
 }
