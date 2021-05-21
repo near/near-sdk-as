8fc95342657aacfe827c7c5dbb45ddbae5b90f58
Ashutosh Varma <ashutoshvarma11@live.com>
2021-05-21 22:30:05 +0530

    docs(sim-ffi): add docstrings from rust crate

--- near-sdk-sim/src/index.ts
+++ near-sdk-sim/src/index.ts
@@ -10,6 +10,11 @@ import { DEFAULT_RUNTIME_CONFIG } from "./config";
 // https://github.com/near/near-sdk-rs/blob/5a8f4dcac44598db19532cdbd6c492bd42e1e777/near-sdk-sim/src/cache.rs#L25
 process.env["CARGO_MANIFEST_DIR"] = "sim-ffi";
 
+/**
+ * The simulator takes an optional GenesisConfig, which sets up the fees and other settings.
+ * It returns the `master_account` which can then create accounts and deploy contracts.
+ * @param genesis_config Optional config to setup the fees and other settings
+ */
 export function init_simulator(genesis_config?: GenesisConfig): UserAccount {
   if (genesis_config) {
     if (!genesis_config.runtime_config)
@@ -21,12 +26,27 @@ export function init_simulator(genesis_config?: GenesisConfig): UserAccount {
 }
 
 // utils
+
+/**
+ * Convert to given value to yocto unit (10^24)
+ * Helpful in calculating gas and user balance
+ * @param value number string to convert
+ * @returns number in yocto unit
+ */
 export function to_yocto(value: string): string {
   return sim.$to_yocto(value);
 }
 
 // Exports
+
+/**
+ * Default Gas value
+ */
 export const DEFAULT_GAS = sim.$DEFAULT_GAS;
+
+/**
+ * Default storage amount
+ */
 export const STORAGE_AMOUNT = sim.$STORAGE_AMOUNT;
 export { UserAccount, UserTransaction } from "./user";
 export { ExecutionResult } from "./outcome";
--- near-sdk-sim/src/user.ts
+++ near-sdk-sim/src/user.ts
@@ -6,15 +6,27 @@ import * as fs from "fs";
 import { join } from "path";
 import { ExecutionResult } from "./outcome";
 
+/**
+ * A user that can sign transactions.  It includes a signer and an account id.
+ */
 export class UserAccount extends RustRef {
+  /**
+   * Returns the `account_id` of the user
+   */
   account_id(): AccountId {
     return sim.$ua$account_id(this.ref);
   }
 
+  /**
+   *  Look up the account information on chain.
+   */
   account(): Account | null {
     return JSON.parse(sim.$ua$account(this.ref));
   }
 
+  /**
+   * Transfer yoctoNear to another account
+   */
   transfer(to: string, deposit: string): ExecutionResult {
     return new ExecutionResult(sim.$ua$transfer(this.ref, to, deposit));
   }
@@ -34,6 +46,10 @@ export class UserAccount extends RustRef {
     );
   }
 
+  /**
+   * Deploy a contract and create its account for `account_id`.
+   * @returns
+   */
   deploy(
     wasm_bytes: Uint8Array | string | string[],
     account_id: string,
@@ -50,6 +66,9 @@ export class UserAccount extends RustRef {
     );
   }
 
+  /**
+   * Deploy a contract and in the same transaction call its initialization method.
+   */
   deploy_and_init(
     wasm_bytes: Uint8Array | string | string[],
     account_id: string,
@@ -80,12 +99,18 @@ export class UserAccount extends RustRef {
     );
   }
 
+  /**
+   * Create a user transaction to `receiver_id` to be signed the current user
+   */
   create_transaction(receiver_id: string): UserTransaction {
     return new UserTransaction(
       sim.$ua$create_transaction(this.ref, receiver_id)
     );
   }
 
+  /**
+   * Call a view method on a contract.
+   */
   view(account_id: string, method: string, args: string | object = "{}"): any {
     if (!(typeof args === "string")) {
       args = JSON.stringify(args);
@@ -93,10 +118,16 @@ export class UserAccount extends RustRef {
     return JSON.parse(sim.$ua$view(this.ref, account_id, method, args));
   }
 
+  /**
+   * Call a view method on a self
+   */
   view_self(method: string, args: string | object = "{}"): any {
     return this.view(this.account_id(), method, args);
   }
 
+  /**
+   * Creates a user and is signed by the `signer_user`
+   */
   create_user_from(
     signer_user: UserAccount,
     account_id: string,
@@ -112,6 +143,9 @@ export class UserAccount extends RustRef {
     );
   }
 
+  /**
+   * Create a new user where the signer is this user account
+   */
   create_user(account_id: string, initial_balance: string): UserAccount {
     return new UserAccount(
       sim.$ua$create_user(this.ref, account_id, initial_balance)
@@ -119,6 +153,10 @@ export class UserAccount extends RustRef {
   }
 }
 
+/**
+ * A transaction to be signed by the user which created it.
+ * Multiple actions can be chained together and then signed and submitted to be executed.
+ */
 export class UserTransaction extends RustRef {
   /**
    * Sign and execute the transaction
@@ -128,16 +166,29 @@ export class UserTransaction extends RustRef {
     return new ExecutionResult(sim.$ut$submit(this.ref));
   }
 
+  /**
+   * Create account for the receiver of the transaction.
+   */
   create_account(): UserTransaction {
     this.ref = sim.$ut$create_account(this.ref);
     return this;
   }
 
+  /**
+   * Deploy WASM binary
+   */
   deploy_contract(wasm_bytes: Buffer): UserTransaction {
     this.ref = sim.$ut$deploy_contract(this.ref, wasm_bytes);
     return this;
   }
 
+  /**
+   * Execute contract call to receiver
+   * @param method methods name to call
+   * @param args args that will be passed to method
+   * @param gas gas amount for transaction
+   * @param deposit deposit amount
+   */
   function_call(
     method: string,
     args: string,
@@ -148,26 +199,41 @@ export class UserTransaction extends RustRef {
     return this;
   }
 
+  /**
+   * Transfer deposit to receiver
+   */
   transfer(deposit: string): UserTransaction {
     this.ref = sim.$ut$transfer(this.ref, deposit);
     return this;
   }
 
+  /**
+   * Express interest in becoming a validator
+   */
   stake(deposit: string, public_key: string): UserTransaction {
     this.ref = sim.$ut$stake(this.ref, deposit, public_key);
     return this;
   }
 
+  /**
+   * Add access key, either FunctionCall or FullAccess
+   */
   add_key(public_key: string, access_key: string): UserTransaction {
     this.ref = sim.$ut$add_key(this.ref, public_key, access_key);
     return this;
   }
 
+  /**
+   * Delete an access key
+   */
   delete_key(public_key: string): UserTransaction {
     this.ref = sim.$ut$delete_key(this.ref, public_key);
     return this;
   }
 
+  /**
+   * Delete an account and send remaining balance to `beneficiary_id`
+   */
   delete_account(beneficiary_id: string): UserTransaction {
     this.ref = sim.$ut$delete_account(this.ref, beneficiary_id);
     return this;
